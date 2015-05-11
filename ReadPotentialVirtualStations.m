6b5357c3685d6c8f16f93fffb11c50f289cff86a
stevetuozzolo <stephen.tuozzolo@gmail.com>
2015-05-11 13:20:39 -0400

    Update ReadPotentialVirtualStations.m
    Changed the script to read the shapefile and get attributes for variables VS and S from the shapefile

--- ReadPotentialVirtualStations.m
+++ ReadPotentialVirtualStations.m
@@ -1,25 +1,15 @@
 function [VS] = ReadPotentialVirtualStations(fname)
 
-[~,~,DataAndHeader]=xlsread(fname);
+S=shaperead(fname);
 
-Nsta=size(DataAndHeader,1)-1; % first row is header
-Ncol=size(DataAndHeader,2);
-for i=1:Nsta,
-    for j=1:Ncol,
-        Data{i,j}=DataAndHeader{i+1,j};
-    end
+for i=1:length(S)
+VS(i).ID=S(i).Station_ID;
+Str=VS(i).ID;
+VS(i).Id=sscanf(Str(14:end),'%g');
+VS(i).Lat=nanmean(S(i).Y);
+VS(i).Lon=nanmean(S(i).X);
+VS(i).Width=S(i).RivWidth;
+VS(i).Pass=S(i).Pass_Num;
 end
 
-Nsta=length(Data);
-for i=1:Nsta,
-    VS(i).ID=Data{i,1};
-    VS(i).Lat=Data{i,2};
-    VS(i).Lon=Data{i,3};
-    VS(i).Width=Data{i,4};
-    VS(i).Pass=Data{i,5};    
-%     VS.Exclude(i)=Data{i,6};
-%     VS.MinLat(i)=Data{i,7};
-%     VS.MaxLat(i)=Data{i,8};
-%     VS.Gage{i}=Data{i,9};
-%     VS.Offset(i)=Data{i,10};
 end
